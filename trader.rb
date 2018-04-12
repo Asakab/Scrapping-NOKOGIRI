2d9fd503b7a7debad627bcb975d360b2f2d15f4e
Selma <37739380+Asakab@users.noreply.github.com>
2018-04-12 21:54:23 +0200

    Add files via upload

--- trader.rb
+++ trader.rb
@@ -1,1 +1,25 @@
-trader.rb
+
+
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+
+def scrap_crypto_money
+
+	page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/")) 
+	price = page.css('a.price').each do |el|
+   puts el.text
+
+	price = []
+ 
+		prices = el['data-usd'] 
+	 puts prices 
+
+ 	end
+ 
+ 
+
+end
+ 
+ scrap_crypto_money
